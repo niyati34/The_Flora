d89991dfa13990c1da306b3c8aca45de6d661334
niyati34 <niyati4201@gmail.com>
2025-06-20 20:20:00 +0530

    wallet flow

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import analytics from './utils/analytics'
+
+// Initialize analytics early
+analytics.init()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
