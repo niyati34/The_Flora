493683b81e0081518d1de3ad2f1a88b44cd1c4d0
niyati34 <niyati4201@gmail.com>
2025-06-26 09:15:00 +0530

    core: init analytics early and normalize formatting

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import analytics from './utils/analytics'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import analytics from "./utils/analytics";
 
 // Initialize analytics early
-analytics.init()
+analytics.init();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
